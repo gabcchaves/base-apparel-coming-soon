d0c484b6ee89a9477bc6327df99ba0e5b2e363f6
Gabriel Costa Chaves <gabcchaves@zohomail.com>
2022-08-20 18:52:35 -0300

    Finished.

--- index.js
+++ index.js
@@ -2,14 +2,26 @@
 
 // Validate e-mail address
 function validateEmail() {
+
 	if ( /.+@.+\..+/.test(document.querySelector("#email-input").value) ) {
+		if (document.querySelector("main small") !== null) {
+			document.querySelector("main").removeChild(document.querySelector("main small"));
+			document.querySelector("main form input[type=email]").style.background = "none";
+		}
 		return true;
 	} else {
-		document.querySelector("main").appendChild(document.createElement("small"));
-		document.querySelector("main :last-child").innerHTML = "Please provide a valid email";
-		document.querySelector("main :last-child").style.color = "var(--soft-red)";
+		let errorMsg = document.createElement("small");
+		errorMsg.innerHTML = "Please provide a valid email";
+		errorMsg.style.color = "var(--desaturated-red)";
+		errorMsg.style.width = "100%";
+		errorMsg.style.padding = "4% 5%";
+		errorMsg.style.display = "block";
+		document.querySelector("main").appendChild(errorMsg);
 		document.querySelector("main form input[type=email]")
 			.style.backgroundImage = 'url("images/icon-error.svg")';
-		console.log("hi");
+		document.querySelector("main form input[type=email]")
+			.style.backgroundRepeat = "no-repeat";
+		document.querySelector("main form input[type=email]")
+			.style.backgroundPosition = "78% center";
 	}
 }
